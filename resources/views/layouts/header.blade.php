e271c3f0727029a12b845fb4565ad5d568208b62
imai_yuki <imakuni.2410@gmail.com>
2023-04-17 17:57:15 +0900

    ヘッダーのcss追加

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,13 @@
-<header>
-    <h1><a href="{{ route('home.index') }}">ホテル予約サイト</a></h1>
-    <nav>
-        <ul>
-            <li><a href="{{ route('feature.index') }}">ホテルの魅力</a></li>
-            <li><a href="{{ route('stay.index') }}">宿泊プラン</a></li>
-            <li><a href="{{ route('restaurant.index') }}">レストラン</a></li>
-            <li><a href="{{ route('banquet.index') }}">会議・宴会</a></li>
-            <li><a href="{{ route('facility.index') }}">施設案内</a></li>
-            <li><a href="{{ route('access.index') }}">アクセス</a></li>
+<header id="header-wrapper">
+    <h1 class="header-heading"><a class="header-heading__title" href="{{ route('home.index') }}">ホテル予約サイト</a></h1>
+    <nav class="header-nav">
+        <ul class="header-nav-wrapper">
+            <li class="header-nav__list"><a class="header-nav__list__a" href="{{ route('feature.index') }}">ホテルの魅力</a></li>
+            <li class="header-nav__list"><a class="header-nav__list__a" href="{{ route('stay.index') }}">宿泊プラン</a></li>
+            <li class="header-nav__list"><a class="header-nav__list__a" href="{{ route('restaurant.index') }}">レストラン</a></li>
+            <li class="header-nav__list"><a class="header-nav__list__a" href="{{ route('banquet.index') }}">会議・宴会</a></li>
+            <li class="header-nav__list"><a class="header-nav__list__a" href="{{ route('facility.index') }}">施設案内</a></li>
+            <li class="header-nav__list"><a class="header-nav__list__a" href="{{ route('access.index') }}">アクセス</a></li>
         </ul>
     </nav>
 </header>
