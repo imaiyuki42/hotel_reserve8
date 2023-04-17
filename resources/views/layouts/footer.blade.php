dd34f40b719fa3fc25da000201acc4e52b65293a
imai_yuki <imakuni.2410@gmail.com>
2023-04-17 18:49:29 +0900

    フッターのcss実装

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
-<footer>
-    <nav>
-        <ul>
-            <li><a href="{{ route('feature.index') }}">ホテルの魅力</a></li>
-            <li><a href="{{ route('stay.index') }}">宿泊プラン</a></li>
-            <li><a href="{{ route('restaurant.index') }}">レストラン</a></li>
-            <li><a href="{{ route('banquet.index') }}">会議・宴会</a></li>
-            <li><a href="{{ route('facility.index') }}">施設案内</a></li>
-            <li><a href="{{ route('access.index') }}">アクセス</a></li>
+<footer id="footer-wrapper">
+    <nav class="footer-nav">
+        <ul class="footer-nav-wrapper">
+            <li class="footer-nav__list"><a class="footer-nav__list__a" href="{{ route('feature.index') }}">ホテルの魅力</a></li>
+            <li class="footer-nav__list"><a class="footer-nav__list__a" href="{{ route('stay.index') }}">宿泊プラン</a></li>
+            <li class="footer-nav__list"><a class="footer-nav__list__a" href="{{ route('restaurant.index') }}">レストラン</a></li>
+            <li class="footer-nav__list"><a class="footer-nav__list__a" href="{{ route('banquet.index') }}">会議・宴会</a></li>
+            <li class="footer-nav__list"><a class="footer-nav__list__a" href="{{ route('facility.index') }}">施設案内</a></li>
+            <li class="footer-nav__list"><a class="footer-nav__list__a" href="{{ route('access.index') }}">アクセス</a></li>
         </ul>
     </nav>
 
